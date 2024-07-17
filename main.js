87159ee85601725cf779377bfe0cd708a8a233f5
GrabovyiS <graboviys@yandex.ru>
2024-07-17 14:47:55 +0300

    init

---
+++ main.js
@@ -0,0 +1,11 @@
+// set up event listeners
+
+async function getWeather(location) {}
+
+async function getProcessedWeather(location) {}
+
+function renderWeather(weather) {}
+
+function showLoader() {}
+
+function hideLoader() {}
