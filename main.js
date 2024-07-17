34a426d4411baeb8d967c693d6d987e7d414aa91
GrabovyiS <graboviys@yandex.ru>
2024-07-17 15:53:09 +0300

    feat: fetch and display weather in browser

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 const form = document.querySelector('form');
+const weatherCard = document.querySelector('.weather-card');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -29,10 +30,9 @@ function getWeather(location) {
     });
 }
 
-async function getProcessedWeather(location) {}
-
 function renderWeather() {
   const location = input.value;
+  console.log('rendering');
   showLoader();
   getWeather(location)
     .finally(() => hideLoader())
@@ -44,12 +44,59 @@ function renderWeather() {
     });
 }
 
-function showLoader() {}
+function showLoader() {
+  weatherCard.textContent = '';
+  const loader = document.createElement('div');
+  loader.classList.add('loader');
+  weatherCard.appendChild(loader);
+}
 
-function hideLoader() {}
+function hideLoader() {
+  weatherCard.textContent = '';
+}
 
 function showError(err) {
   console.log(err);
 }
 
-function renderWeatherCard() {}
+function renderWeatherCard(weatherData) {
+  const addressField = document.createElement('div');
+  addressField.classList.add('weather-field');
+
+  const addressHeading = document.createElement('h2');
+  addressHeading.textContent = 'Address:';
+
+  const addressText = document.createElement('p');
+  addressText.textContent = weatherData.resolvedAddress;
+
+  addressField.appendChild(addressHeading);
+  addressField.appendChild(addressText);
+
+  const conditionsField = document.createElement('div');
+  conditionsField.classList.add('weather-field');
+
+  const conditionsHeading = document.createElement('h2');
+  conditionsHeading.textContent = 'Conditions:';
+
+  const conditionsText = document.createElement('p');
+  conditionsText.textContent = weatherData.currentConditions.conditions;
+
+  conditionsField.appendChild(conditionsHeading);
+  conditionsField.appendChild(conditionsText);
+
+  const temperatureField = document.createElement('div');
+  temperatureField.classList.add('weather-field');
+
+  const temperatureHeading = document.createElement('h2');
+  temperatureHeading.textContent = 'Temperature:';
+
+  const temperatureText = document.createElement('p');
+  temperatureText.textContent = weatherData.currentConditions.temp;
+
+  temperatureField.appendChild(temperatureHeading);
+  temperatureField.appendChild(temperatureText);
+
+  weatherCard.appendChild(addressField);
+  weatherCard.appendChild(conditionsField);
+  weatherCard.appendChild(temperatureField);
+}
