8e2156a7fd840fabb9a5dfc95ec30f1dccebd978
GrabovyiS <graboviys@yandex.ru>
2024-07-17 15:21:49 +0300

    feat: fetch and display weather in console

--- main.js
+++ main.js
@@ -1,11 +1,55 @@
 // set up event listeners
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const form = document.querySelector('form');
 
-async function getWeather(location) {}
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  renderWeather();
+});
+
+function getWeather(location) {
+  return fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&elements=datetime%2Cname%2Ctemp%2Chumidity%2Cconditions&key=LH7L9BQJ8VA93UEZG77J49J3B&contentType=json`,
+    { mode: 'cors' }
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Bad request. Status code: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((result) => {
+      const weather = {
+        address: result.address,
+        resolvedAddress: result.resolvedAddress,
+        currentConditions: result.currentConditions,
+      };
+      return weather;
+    });
+}
 
 async function getProcessedWeather(location) {}
 
-function renderWeather(weather) {}
+function renderWeather() {
+  const location = input.value;
+  showLoader();
+  getWeather(location)
+    .finally(() => hideLoader())
+    .then((weather) => {
+      renderWeatherCard(weather);
+    })
+    .catch((err) => {
+      showError(err);
+    });
+}
 
 function showLoader() {}
 
 function hideLoader() {}
+
+function showError(err) {
+  console.log(err);
+}
+
+function renderWeatherCard() {}
